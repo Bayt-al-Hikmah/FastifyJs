ff513f010c5905cd14ab721c336f0c9273b54f08
Anunnaki <92318629+AliTigui@users.noreply.github.com>
2025-11-03 17:35:33 +0100

    Add CSRF protection to contact form POST route

--- Workshop2/Example/Example5/routes/contact_csrf.js
+++ Workshop2/Example/Example5/routes/contact_csrf.js
@@ -16,9 +16,8 @@ module.exports = async (fastify, opts) => {
     return reply.view('contact_csrf', { csrfToken: token })
   })
 
-  fastify.post('/contact_csrf', { schema: contactSchema }, async (request, reply) => {
+  fastify.post('/contact_csrf', { schema: contactSchema,preHandler: fastify.csrfProtection }, async (request, reply) => {
     const { name, message } = request.body
-    fastify.log.info(`Received from ${name}: ${message}`)
     const token = await reply.generateCsrf()
     return reply.view('contact_csrf', { submittedName: name, csrfToken: token })
   })
